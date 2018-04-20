f46b53e6640b5e86ee8d7e7daf1afd3104570726
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-20 13:43:59 +0700

    Выделены методы (функции), добавлено форматирование вывода

--- lec_1_hw_2_3/Program.cs
+++ lec_1_hw_2_3/Program.cs
@@ -9,76 +9,88 @@ namespace lec_1_hw_2_3
             do
             {
                 Console.Clear();
-                double leftOperand, rightOperand, result=0;
+                double leftOperand, rightOperand, result;
                 string operation;
-                bool leftOperandIsOk, rightOperandIsOk, operationIsOk;
-                do
-                {
-                    leftOperandIsOk = false;
-                    rightOperandIsOk = false;
-                    operationIsOk = false;
-                    leftOperand = 0;
-                    rightOperand = 0;
-                    operation = "";
-                    try
-                    {
-                        Console.Write("Введите левый операнд:");
-                        leftOperand = Convert.ToDouble(Console.ReadLine());
-                        leftOperandIsOk = true;
-                        while(!operationIsOk)
-                        {
-                            Console.Write("Введите знак операции (+, -, *, /, %):");
-                            operation = Console.ReadLine();
-                            switch (operation)
-                            {
-                                case "+":
-                                case "-":
-                                case "*":
-                                case "/":
-                                case "%":
-                                    operationIsOk = true;
-                                    break;
-                                default:
-                                    Console.WriteLine("Не существующая операция!!!");
-                                    break;
-                            }
-                        }
-                        while (!rightOperandIsOk)
-                        {
-                            Console.Write("Введите правый операнд:");
-                            rightOperand = Convert.ToDouble(Console.ReadLine());
-                            if ((operation == "/" || operation == "%") && rightOperand == 0)
-                                Console.WriteLine("Неверное значение правого операнда - попытка деления на ноль!!!");
-                            else
-                                rightOperandIsOk = true;
-                        }
-                    }
-                    catch(FormatException)
-                    {
-                        Console.WriteLine("Операнд(ы) не является(ются) числом(ами)!!!!");
-                    }
-                } while (!(leftOperandIsOk && rightOperandIsOk && operationIsOk));
-                switch(operation)
-                {
-                    case "+":
-                        result = leftOperand + rightOperand;
-                        break;
-                    case "-":
-                        result = leftOperand - rightOperand;
-                        break;
-                    case "*":
-                        result = leftOperand * rightOperand;
-                        break;
-                    case "/":
-                        result = leftOperand / rightOperand;
-                        break;
-                    case "%":
-                        result = Math.IEEERemainder(leftOperand, rightOperand);
-                        break;
-                }
-                Console.WriteLine("({0}) {1} ({2}) = ({3})", leftOperand, operation, rightOperand, result);
+                InputData(out leftOperand, out operation, out rightOperand);
+                result = Calculate(leftOperand, operation, rightOperand);
+                Console.WriteLine("({0}) {1} ({2}) = ({3:f2})", leftOperand, operation, rightOperand, result);
                 Console.Write("Продолжить: Да (1), Нет (any other)...");
             } while(Console.ReadLine()=="1");
+        }
+
+        static void InputData(out double leftOperand, out string operation, out double rightOperand)
+        {
+            bool leftOperandIsOk, rightOperandIsOk, operationIsOk;
+            do
+            {
+                leftOperandIsOk = false;
+                rightOperandIsOk = false;
+                operationIsOk = false;
+                leftOperand = 0;
+                rightOperand = 0;
+                operation = string.Empty;
+                try
+                {
+                    Console.Write("Введите левый операнд:");
+                    leftOperand = Convert.ToDouble(Console.ReadLine());
+                    leftOperandIsOk = true;
+                    while (!operationIsOk)
+                    {
+                        Console.Write("Введите знак операции (+, -, *, /, %):");
+                        operation = Console.ReadLine();
+                        switch (operation)
+                        {
+                            case "+":
+                            case "-":
+                            case "*":
+                            case "/":
+                            case "%":
+                                operationIsOk = true;
+                                break;
+                            default:
+                                Console.WriteLine("Не существующая операция!!!");
+                                break;
+                        }
+                    }
+                    while (!rightOperandIsOk)
+                    {
+                        Console.Write("Введите правый операнд:");
+                        rightOperand = Convert.ToDouble(Console.ReadLine());
+                        if ((operation == "/" || operation == "%") && rightOperand == 0)
+                            Console.WriteLine("Неверное значение правого операнда - попытка деления на ноль!!!");
+                        else
+                            rightOperandIsOk = true;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Операнд(ы) не является(ются) числом(ами)!!!!");
+                }
+            } while (!(leftOperandIsOk && rightOperandIsOk && operationIsOk));
+        }
+
+        static double Calculate(double leftOperand, string operation, double rightOperand)
+        {
+            double result=0;
+            switch (operation)
+            {
+                case "+":
+                    result = leftOperand + rightOperand;
+                    break;
+                case "-":
+                    result = leftOperand - rightOperand;
+                    break;
+                case "*":
+                    result = leftOperand * rightOperand;
+                    break;
+                case "/":
+                    result = leftOperand / rightOperand;
+                    break;
+                case "%":
+                    result = Math.IEEERemainder(leftOperand, rightOperand);
+                    break;
+            }
+            return result;
         }
     }
 }
